6e1e203e83fa150e49f44ccb9911f1821df3c3be
godzeus69 <79259866+godzeus69@users.noreply.github.com>
2021-02-18 20:10:45 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,7 +8,7 @@
     $id = $update["message"]["from"]["id"];
     $username = $update["message"]["from"]["username"];
     $firstname = $update["message"]["from"]["first_name"];
-    $bot_name = "" ;//your bot name
+    $bot_name = "Zeus" ;//your bot name
  /// for broadcasting in Channel
 $channel_id = "-100xxxxxxxxxx"; 
 
@@ -151,7 +151,7 @@ else {
    }
     
 
-    //Wheather API
+    //Wheather 
 if(strpos($message, "/weather") === 0){
         $location = substr($message, 9);
    $curl = curl_init();
@@ -204,7 +204,7 @@ else {
 }
     }
 
-///Github User API
+///Github User 
 if(strpos($message, "/git") === 0){
   $git = substr($message, 5);
    $curl = curl_init();
@@ -319,14 +319,14 @@ Checked By @$username ");
 
      ///Send Message (Global)
     function send_message($chat_id, $message){
-        $apiToken =  "API_TOKEN";
+        $apiToken =  "1699905911:AAGCWJVtgFXC4e6VlFd_SVsacW8rxFqq26c";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
     }
     
 //Send Messages with Markdown (Global)
       function send_MDmessage($chat_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1699905911:AAGCWJVtgFXC4e6VlFd_SVsacW8rxFqq26c";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text&parse_mode=Markdown");
     }
@@ -334,13 +334,13 @@ Checked By @$username ");
 
 ///Send Message to Channel
       function send_Cmessage($channel_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1699905911:AAGCWJVtgFXC4e6VlFd_SVsacW8rxFqq26c";
         $text = urlencode($message);
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$channel_id&text=$text");
     }
 
  function sendDice($chat_id, $message){
-       $apiToken =  "API_TOKEN";
+       $apiToken =  "1699905911:AAGCWJVtgFXC4e6VlFd_SVsacW8rxFqq26c";
         file_get_contents("https://api.telegram.org/bot$apiToken/sendDice?chat_id=$chat_id&emoji=$message");
     }
 ?>
